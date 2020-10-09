57c7c3d54f95e52596d4a90d8b3e7f751c6662aa
Ishaan Verma <ishaanverma97@gmail.com>
2020-10-09 15:22:43 +0530

    added stack navigation for each tab

--- src/navigation/BottomNavigation.js
+++ src/navigation/BottomNavigation.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Dashboards from '../screens/Dashboards';
-import Account from '../screens/Account';
-import Notification from '../screens/Notification';
+import {
+  DashboardStackNavigator,
+  AccountStackNavigator,
+  NotificationStackNavigator,
+} from './StackNavigation';
 
 const Tab = createBottomTabNavigator();
 
 const MainBottomNavigator = () => {
+  const HomeScreenName = 'Home';
+  const AccountScreenName = 'Account';
+  const NotificationScreenName = 'Notifications';
+
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
-          if (route.name === 'Dashboards') {
+          if (route.name === HomeScreenName) {
             iconName = 'home';
-          } else if (route.name === 'Account') {
+          } else if (route.name === AccountScreenName) {
             iconName = 'account-circle';
-          } else if (route.name === 'Notifications') {
+          } else if (route.name === NotificationScreenName) {
             iconName = 'bell';
           }
           return (
             <MaterialCommunityIcons name={iconName} size={size} color={color} />
           );
         },
+        headerShown: true,
       })}>
-      <Tab.Screen name="Dashboards" component={Dashboards} />
-      <Tab.Screen name="Account" component={Account} />
-      <Tab.Screen name="Notifications" component={Notification} />
+      <Tab.Screen name={HomeScreenName} component={DashboardStackNavigator} />
+      <Tab.Screen name={AccountScreenName} component={AccountStackNavigator} />
+      <Tab.Screen
+        name={NotificationScreenName}
+        component={NotificationStackNavigator}
+      />
     </Tab.Navigator>
   );
 };
--- src/navigation/StackNavigation.js
+++ src/navigation/StackNavigation.js
@@ -3,6 +3,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { MainBottomNavigator } from '../navigation/BottomNavigation';
 import Login from '../screens/Login';
 import Splash from '../screens/Splash';
+import Dashboards from '../screens/Dashboards';
+import Account from '../screens/Account';
+import Notification from '../screens/Notification';
 
 const Stack = createStackNavigator();
 
@@ -23,4 +26,33 @@ const MainStackNavigator = ({ authState }) => {
   );
 };
 
-export { MainStackNavigator };
+const DashboardStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Dashboards} />
+    </Stack.Navigator>
+  );
+};
+
+const AccountStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Account" component={Account} />
+    </Stack.Navigator>
+  );
+};
+
+const NotificationStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Notifications" component={Notification} />
+    </Stack.Navigator>
+  );
+};
+
+export {
+  MainStackNavigator,
+  DashboardStackNavigator,
+  AccountStackNavigator,
+  NotificationStackNavigator,
+};
