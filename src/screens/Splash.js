2288fcc670e0f4d2aa1b1878fa8aefa03f581033
Ishaan Verma <ishaanverma97@gmail.com>
2020-11-18 16:34:34 +0530

    updated splash screen

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
+import DefaultView from '../components/default/DefaultView';
 
 const Splash = () => {
   return (
-    <View>
-      <Text>Splash</Text>
-    </View>
+    <DefaultView>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/cognitensor-logo.png')}
+        />
+      </View>
+    </DefaultView>
   );
 };
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    alignItems: 'center',
+  },
+  image: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Splash;
